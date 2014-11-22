021bffcdeec7d7313b1f98395ff8afad482e2b21
Mahdi Khanalizadeh <mahdi@khanalizadeh.com>
2014-11-22 21:15:32 +0100

    added Winamp extensions

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 #include <string.h>
 
 
-static char* genres[] = { "Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk", "Grunge", "Hip-Hop", "Jazz", "Metal", "New Age",
+static char* genres[] = {
+	// Original 0 to 79
+	"Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk", "Grunge", "Hip-Hop", "Jazz", "Metal", "New Age",
 	"Oldies", "Other", "Pop", "Rhythm and Blues", "Rap", "Reggae", "Rock", "Techno", "Industrial", "Alternative", "Ska", "Death Metal",
 	"Pranks", "Soundtrack", "Euro-Techno", "Ambient", "Trip-Hop", "Vocal", "Jazz & Funk", "Fusion", "Trance", "Classical", "Instrumental",
 	"Acid", "House", "Game", "Sound Clip", "Gospel", "Noise", "Alternative Rock", "Bass", "Soul", "Punk rock", "Space", "Meditative",
@@ -12,6 +14,18 @@ static char* genres[] = { "Blues", "Classic Rock", "Country", "Dance", "Disco", 
 	"Dream", "Southern Rock", "Comedy", "Cult", "Gangsta", "Top 40", "Christian Rap", "Pop/Funk", "Jungle", "Native American", "Cabaret",
 	"New Wave", "Psychedelic", "Rave", "Showtunes", "Trailer", "Lo-Fi", "Tribal", "Acid Punk", "Acid Jazz", "Polka", "Retro", "Musical",
 	"Rock & Roll", "Hard Rock",
+	// Winamp extensions 80 to 191
+	"Folk", "Folk-Rock", "National Folk", "Swing", "Fast Fusion", "Bebop", "Latin", "Revival", "Celtic", "Bluegrass", "Avantgarde",
+	"Gothic Rock", "Progressive Rock", "Psychedelic Rock", "Symphonic Rock", "Slow Rock", "Big Band", "Chorus", "Easy Listening", "Acoustic",
+	"Humour", "Speech", "Chanson", "Opera", "Chamber Music", "Sonata", "Symphony", "Booty Bass", "Primus", "Porn groove", "Satire",
+	"Slow Jam", "Club", "Tango", "Samba", "Folklore", "Ballad", "Power Ballad", "Rhythmic Soul", "Freestyle", "Duet", "Punk rock",
+	"Drum Solo", "A capella", "Euro-House", "Dance Hall", "Goa", "Drum & Bass", "Club-House", "Hardcore Techno", "Terror", "Indie",
+	"BritPop", "Afro-punk", "Polsk Punk", "Beat", "Christian Gangsta Rap", "Heavy Metal", "Black Metal", "Crossover", "Contemporary Christian",
+	"Christian Rock", "Merengue", "Salsa", "Thrash Metal", "Anime", "Jpop", "Synthpop", "Abstract", "Art Rock", "Baroque", "Bhangra",
+	"Big Beat", "Breakbeat", "Chillout", "Downtempo", "Dub", "EBM", "Eclectic", "Electro", "Electroclash", "Emo", "Experimental", "Garage",
+	"Global", "IDM", "Illbient", "Industro-Goth", "Jam Band", "Krautrock", "Leftfield", "Lounge", "Math Rock", "New Romantic", "Nu-Breakz",
+	"Post-Punk", "Post-Rock", "Psytrance", "Shoegaze", "Space Rock", "Trop Rock", "World Music", "Neoclassical", "Audiobook", "Audio Theatre",
+	"Neue Deutsche Welle", "Podcast", "Indie Rock", "G-Funk", "Dubstep", "Garage Rock", "Psybient"
 };
 
 
@@ -53,9 +67,9 @@ int main(int argc, char* argv[])
 	{
 		printf("Track:   \"%d\"\n", (int)*(tag+126));
 	}
-	if ((unsigned char)tag[127] > 79)
+	if ((unsigned char)tag[127] > 191)
 		tag[127] = 12; // set to 'Other'
-	printf("Genre:   \"%s\"\n", genres[(size_t)tag[127]]);
+	printf("Genre:   \"%s\"\n", genres[(size_t)(unsigned char)tag[127]]);
 
 	return EXIT_SUCCESS;
 }
